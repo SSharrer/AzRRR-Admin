f29dc47f4d2d579b2bbc01ce7e77d1f31063d048
RYANWORKLAPTOP\ryanr <ryanrichard72@hotmail.com>
2024-08-23 11:02:59 -0500

    add environment prod file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    webApiBaseUrl: 'https://rrrapi.azurewebsites.net/api/',
+    apiTaskKey: 'A12C1D96-5BA2-465E-9FD1-A240331F2712'
+  };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    webApiBaseUrl: 'http://localhost:47793/api/'
+    webApiBaseUrl: 'http://localhost:47793/api/',
+    apiTaskKey: 'A12C1D96-5BA2-465E-9FD1-A240331F2712'
 };
