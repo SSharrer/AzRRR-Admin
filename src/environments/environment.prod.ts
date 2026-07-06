486bf4db83e9c274acbd63e94afbe194657d3241
RYANWORKLAPTOP\ryanr <ryanrichard72@hotmail.com>
2026-07-06 14:37:36 -0500

    enviropnment and package updtes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    webApiBaseUrl: 'https://rrrapi.azurewebsites.net/api/',
+    //webApiBaseUrl: 'https://rrrapi.azurewebsites.net/api/',
+      webApiBaseUrl: 'https://rrrappapi.azurewebsites.net/api/',
     apiTaskKey: 'A12C1D96-5BA2-465E-9FD1-A240331F2712'
   };
